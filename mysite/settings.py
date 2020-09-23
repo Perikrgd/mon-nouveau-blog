42a3c17fc58297d9e04aafbaef8b843c6b42387e
Rigoudy <rigoudy_adm@PBPDSIP12806P.ADLYON2.UNIV-LYON2.FR>
2020-09-23 14:17:38 +0200

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,11 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-=======
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
->>>>>>> f31c6c0f9a82b850639272d43ec68e6b5e5c79b0
+
